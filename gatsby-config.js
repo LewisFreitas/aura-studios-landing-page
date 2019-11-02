ad8a16f44f4ad233d201f09dd2cedb2f59312287
LewisFreitas <luis.freitas344@gmail.com>
2019-11-02 00:08:01 +0000

    Added plugin to load Mailchimp js lib

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,19 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `Aura Studios`,
+    siteHeadline: `Aura Studios | Virtual & Augmented Reality studio`,
+    siteUrl: `https://aurastudios.xyz`,
+    siteDescription: `Creating realities | Creative engineering studio focused on the next computing platform.`,
     siteTitleAlt: `Aura Studios`,
     siteLanguage: `en`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {},
+      options: {
+
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -21,9 +27,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Aura Studios`,
+        short_name: `Aura`,
+        description: `Creating realities | Creative engineering studio focused on the next computing platform.`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
@@ -40,6 +46,13 @@ module.exports = {
             type: `image/png`,
           },
         ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        src: 'http://downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js',
+        onLoad: `() => window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us3.list-manage.com","uuid":"ea9b7c0cb1f4460e20e03ed6c","lid":"0be220cf33","uniqueMethods":true}) })`,
       },
     },
     `gatsby-plugin-offline`,
